9b56b2eee6cea88a381271193dad321674d3aba9
Tilman Blumenbach <tilman+git@ax86.net>
2016-02-17 23:29:08 +0100

    Add copyright headers.

--- toggl_fetch/api.py
+++ toggl_fetch/api.py
@@ -1,4 +1,22 @@
-"""Provides classes to interact with the Toggl.com REST APIs."""
+"""Provides classes to interact with the Toggl.com REST APIs.
+
+This file is part of toggl-fetch, see https://github.com/Tblue/toggl-fetch.
+
+Copyright 2016  Tilman Blumenbach
+
+toggl-fetch is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+toggl-fetch is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with toggl-fetch.  If not, see http://www.gnu.org/licenses/.
+"""
 
 import json
 import logging
--- toggl_fetch/fetch.py
+++ toggl_fetch/fetch.py
@@ -1,3 +1,23 @@
+"""Fetches summary reports from Toggl.com, with automatic date range calculation - console-based frontend.
+
+This file is part of toggl-fetch, see https://github.com/Tblue/toggl-fetch.
+
+Copyright 2016  Tilman Blumenbach
+
+toggl-fetch is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+toggl-fetch is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with toggl-fetch.  If not, see http://www.gnu.org/licenses/.
+"""
+
 import configparser
 import datetime
 import json
